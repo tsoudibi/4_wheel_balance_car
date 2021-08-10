8bedf9aab1a475d2ed959a5c245b295548db2f8e
paul881023 <E94076021@gs.ncku.edu.tw>
2021-08-10 18:16:36 +0800

    sensor mode inprove

--- HX711-multi_0602/motorspeed.h
+++ HX711-multi_0602/motorspeed.h
@@ -8,6 +8,9 @@
 Queue<unsigned long> queue_map_X(5);
 Queue<unsigned long> queue_map_Y(5);
 
+/*set time*/
+unsigned long time_count = 0;
+
 int flag = 0;
 int count = 0;
 
@@ -24,24 +27,22 @@ void speed_control(int map_x_ori,int map_y_ori,int map_X, int map_Y){
   int FlagtoDetectchange = 0;
 
   //left
-  if (map_X - map_x_ori > 600 && FlagtoDetectchange == 0)
+  if (map_X - map_x_ori > 300 && FlagtoDetectchange == 0)
   {
     speed_l++;
     speed_r--;
     FlagtoDetectchange = 1;
   }
   
-  
   //right
-  if(map_X - map_x_ori < -600 && FlagtoDetectchange == 0)
+  if(map_X - map_x_ori < -300 && FlagtoDetectchange == 0)
   {
     speed_l--;
     speed_r++;
     FlagtoDetectchange = 1;
   }
   
-  
-    if (-600 < map_X - map_x_ori < 600 && FlagtoDetectchange == 0) 
+  if (-300 < map_X - map_x_ori < 300 && FlagtoDetectchange == 0) 
   {
     //forward
     if(map_Y - map_y_ori > 300)
@@ -81,11 +82,11 @@ void ResetCoordinate(int map_X,int map_Y)
   {
     if(abs(queue_map_X.peek() - map_X) > 10 && abs(queue_map_Y.peek()- map_Y) > 10)
     {
-    flag = 1;
     //Serial.println("reset");
-    delay(5000);
+    delay(3000);
     map_x_ori = map_X;
     map_y_ori = map_Y;
+    flag = 1;
     }
   }
   //whether come back to initialzation or not
@@ -100,8 +101,10 @@ void ResetCoordinate(int map_X,int map_Y)
         count = 0;
         
       //reset successfully and stop
-      if (count == 3)
+      if (count == 2)
         flag = 0;
+        map_x_ori = 0;
+        map_y_ori = 0;
     }
   }  
 }
@@ -112,7 +115,7 @@ void SpeedOutput(int map_X,int map_Y)
   LocationtoQueue(map_X,map_Y);
   ResetCoordinate(map_X,map_Y);
 
-  unsigned long time_count = 0;
+  
   //靜止
   if (flag == 0)
   {
@@ -123,7 +126,7 @@ void SpeedOutput(int map_X,int map_Y)
   if (flag == 1)
   { 
   /*set time to smooth the speed change*/
-    if(millis()- time_count > 1000)
+    if(millis()- time_count > 500)
     {
       speed_control(map_x_ori,map_y_ori,map_X,map_Y);
       time_count = millis();
@@ -143,6 +146,6 @@ void SpeedOutput(int map_X,int map_Y)
     speed_r = 10;
 
   //transport data to esp32    
-  Serial.println(String(map_X)+","+String(map_Y)+","+String(speed_l)+","+String(speed_r)+","+String(flag));
+  Serial.println(String(map_X - map_x_ori)+","+String(map_Y - map_y_ori)+","+String(speed_l)+","+String(speed_r)+","+String(flag));
 }
 #endif
