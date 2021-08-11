ec0d056f682f6e2e084e67dba7c153421007f4d9
tsoudibi <a220284268@gmail.com>
2021-08-11 15:47:04 +0800

    add camera_control.h

---
+++ esp32_main/camera_control.h
@@ -0,0 +1,60 @@
+#ifndef CAMERA_CONTROL_H
+#define CAMERA_CONTROL_H
+
+#include "esp32_http.h"
+#include "motor_control.h"
+#include "arduino_connect.h"
+#include "pid.h"
+
+#define threshold 20
+
+/* save the HZ of motor should be */
+/*The smallest speed is 7*/
+int cam_HZ_L = 0;
+int cam_HZ_R = 0;
+
+/* store string as char array*/
+char HZ_char_array[50] ;
+
+void camera_control(){
+  /* update data from encoder and sensor */
+  update_data_all();
+
+  /* get HZ command from sever */
+  String HZ_string = http_GET("cam_HZ");
+
+  /* to char array*/
+  int HZ_string_len = HZ_string.length() + 1;   //sensor
+  HZ_string.toCharArray(HZ_char_array, HZ_string_len); //sensor
+  
+  /* split  */
+  char *arr_1;
+  arr_1 = strtok(HZ_char_array, ",");
+  int i = 0;
+  int value_1[3];
+  while (arr_1 != NULL)
+  {
+    value_1[++i] = atoi(arr_1); 
+    arr_1 = strtok(NULL, ",");
+  }
+  
+  cam_HZ_L = value_1[1];
+  cam_HZ_R = value_1[2];
+  
+
+  /* print data and send to server */
+  /*time_now = millis();
+  Serial.println( "[HTTP] post:"+http_POST(HZ_L,HZ_R,ISR_HZ_L,ISR_HZ_R,map_x,map_y)+" ,used time:"+(millis()-time_now));*/
+  
+}
+
+
+int return_command_hz_camera(char which)
+{
+  if (which == 'l')
+    return cam_HZ_L;
+  if (which == 'r')
+    return cam_HZ_R;
+}
+
+#endif /* CAMERA_CONTROL_H */
