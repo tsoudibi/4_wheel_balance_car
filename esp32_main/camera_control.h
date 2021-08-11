e5ed374a7f6a89814c5fab14aefa97c727140826
tsoudibi <a220284268@gmail.com>
2021-08-11 17:23:49 +0800

    add threshold

--- esp32_main/camera_control.h
+++ esp32_main/camera_control.h
@@ -13,6 +13,10 @@
 int cam_HZ_L = 0;
 int cam_HZ_R = 0;
 
+/* backup HZ in case of error*/
+int cam_HZ_L_backup = 0;
+int cam_HZ_R_backup = 0;
+
 /* store string as char array*/
 char HZ_char_array[50] ;
 
@@ -23,11 +27,11 @@ void camera_control(){
   /* get HZ command from sever */
   String HZ_string = http_GET("cam_HZ");
 
-  /* to char array*/
+  /* to char array */
   int HZ_string_len = HZ_string.length() + 1;   //sensor
   HZ_string.toCharArray(HZ_char_array, HZ_string_len); //sensor
   
-  /* split  */
+  /* split */
   char *arr_1;
   arr_1 = strtok(HZ_char_array, ",");
   int i = 0;
@@ -37,9 +41,20 @@ void camera_control(){
     value_1[++i] = atoi(arr_1); 
     arr_1 = strtok(NULL, ",");
   }
+
+  /* if error, use bacup command */
+  if (cam_HZ_L > 100 || cam_HZ_R > 100 || cam_HZ_L < 0 || cam_HZ_R < 0){
+    cam_HZ_L = cam_HZ_L_backup;
+    cam_HZ_R = cam_HZ_R_backup;
+  }else{
+    cam_HZ_L = value_1[1];
+    cam_HZ_R = value_1[2];
+  }
   
-  cam_HZ_L = value_1[1];
-  cam_HZ_R = value_1[2];
+  /* stor backup command */
+  cam_HZ_L_backup = cam_HZ_L;
+  cam_HZ_R_backup = cam_HZ_R;
+  
   
 
   /* print data and send to server */
