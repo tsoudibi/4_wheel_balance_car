5a41680b6f78333bc80acd11d91fbbf5f1191231
paul881023 <E94076021@gs.ncku.edu.tw>
2021-08-13 17:38:54 +0800

    pid

--- esp32_main/pid.h
+++ esp32_main/pid.h
@@ -2,6 +2,7 @@
 #define pid_h
 #include <esp32-hal-dac.h>
 #include "PID_v1.h"
+#include "arduino_connect.h"
 
 /* HZ from encoder */
 int Encoding_HZ_l;
@@ -15,70 +16,67 @@ double Output_r;
 /* output (DAC)*/
 double Output_l_fin;
 double Output_r_fin;
-/* PID parameter */
-/*double kp_l,ki_l,kd_l;
-double kp_r,ki_r,kd_r;*/
-/* for button */
-double kp_l=0.6,ki_l=0.8,kd_l=0;
-double kp_r=0.6,ki_r=0.5,kd_r=0;
-/*for sensor*/
-/*double kp_l=3,ki_l=1.3,kd_l=0;
-double kp_r=3,ki_r=1.2,kd_r=0;*/
-
+/*PID parameter set*/
+/*for load*/
+double kp_l_load = 3,ki_l_load = 1,kd_l_load = 0;
+double kp_r_load = 3,ki_r_load = 1.3,kd_r_load = 0;
+/*for not load*/
+double kp_l_Nload = 0.2,ki_l_Nload = 1,kd_l_Nload = 0;
+double kp_r_Nload = 0.2,ki_r_Nload = 1.2,kd_r_Nload = 0;
+/*others*/
 double input_l ,output_l, setpoint_l;
 double input_r ,output_r, setpoint_r;
 
-/*void get_control_mode(int control_mode_PID)
-{
-  int control_Mode = 0;
-  control_Mode = control_mode_PID;
-  if(control_Mode == 1 || control_Mode == 2)
-  {
-    kp_l=3;ki_l=1.3;kd_l=0;
-    kp_r=3;ki_r=1.2;kd_r=0;
-    Serial.println("mode"+String(control_Mode));
-  }
-  else if(control_Mode == 3)
-  {
-    kp_l=1.6;ki_l=0.6;kd_l=0;
-    kp_r=1.7;ki_r=0.8;kd_r=0;
-    Serial.println("mode"+String(control_Mode));
-  }
-}*/
-
 /* create PID object*/
-PID mypid_left(&input_l, &output_l, &setpoint_l, kp_l, ki_l, kd_l, DIRECT); 
-PID mypid_right(&input_r, &output_r, &setpoint_r, kp_r, ki_r, kd_r, DIRECT); 
-
+/* has load */
+PID mypid_left_load(&input_l, &output_l, &setpoint_l, kp_l_load, ki_l_load, kd_l_load, DIRECT); 
+PID mypid_right_load(&input_r, &output_r, &setpoint_r, kp_r_load, ki_r_load, kd_r_load, DIRECT); 
+/* not has load*/
+PID mypid_left_Nload(&input_l, &output_l, &setpoint_l, kp_l_Nload, ki_l_Nload, kd_l_Nload, DIRECT); 
+PID mypid_right_Nload(&input_r, &output_r, &setpoint_r, kp_r_Nload, ki_r_Nload, kd_r_Nload, DIRECT);
+ 
 /* set up */
 void PID_setup()
 {
-   mypid_right.SetMode(AUTOMATIC);
-   mypid_left.SetMode(AUTOMATIC);
-}
-/*to get mode to change parameter*/
+   /*load*/
+   mypid_right_load.SetMode(AUTOMATIC);
+   mypid_left_load.SetMode(AUTOMATIC);
 
+   /*no load*/
+   mypid_right_Nload.SetMode(AUTOMATIC);
+   mypid_left_Nload.SetMode(AUTOMATIC);
+}
 
 /* caculate PID left */
-void PIDcontrol_left()
+void PIDcontrol_left(int flag_left)
 {
   input_l = Encoding_HZ_l;
   Output_l = output_l;
   setpoint_l = command_HZ_l;
   /* compute */
-  mypid_left.Compute();
+  if(flag_left == 0){//no load
+    mypid_left_Nload.Compute();
+  }
+  else if(flag_left == 1){//has load
+    mypid_left_load.Compute();
+  }
   /* convert output from HZ to DAC*/
   Output_l_fin =  Output_l*0.7058+96.148;
 }
 
 /* caculate PID right */
-void PIDcontrol_right()
+void PIDcontrol_right(int flag_right)
 {
   input_r = Encoding_HZ_r;
   Output_r = output_r;
   setpoint_r = command_HZ_r;
   /* compute */
-  mypid_right.Compute();
+  if(flag_right == 0){//no load
+    mypid_right_Nload.Compute();
+  }
+  else if(flag_right == 1){//has load
+    mypid_right_load.Compute();
+  }
   /* convert output from HZ to DAC*/
   Output_r_fin =  Output_r*0.7539+96.166;
 }
@@ -89,18 +87,17 @@ int get_PID_result(char which){
   if (which == 'r') return Output_r_fin;
 }
 /* gather all data needed and do the PID */
-void PID(int command_hz_L,int command_hz_R,int encoder_hz_L,int encoder_hz_R)
+void PID(int command_hz_L,int command_hz_R,int encoder_hz_L,int encoder_hz_R,int flag)
 {
   /* set parameter needed (command HZ and encoder HZ) */
-  /* let PID make change smoothy */
   /* left */
   command_HZ_l = command_hz_L;
   Encoding_HZ_l = encoder_hz_L;
-  PIDcontrol_left();
+  PIDcontrol_left(flag);
   /*right*/
   command_HZ_r = command_hz_R;
   Encoding_HZ_r = encoder_hz_R;
-  PIDcontrol_right();
+  PIDcontrol_right(flag);
   //Serial.println(String(command_hz_L)+","+String(command_hz_R)+","+String(command_HZ_l)+","+String(command_HZ_r));
 }
 #endif
